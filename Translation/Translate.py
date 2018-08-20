6ad7d096432b0eac725efb3c33ffd78e335204d5
hongxu55 <hong.xu55@pactera.com>
2018-08-20 14:28:36 -0700

    added translation .py

---
+++ Translation/Translate.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#coding: utf8
+
+#Author: hong xu(hong.xu55@pactera.com)
+#Last Modification: 08/20/2018
+
+import pandas as pd
+from google.cloud import translate
+import os
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './ZSProject-94cb8e930aab.json'
+
+def run_quickstart(text):
+  # Imports the Google Cloud client library
+  translate_client = translate.Client()
+  target = 'Zh-cn'
+  translation = translate_client.translate(text, target_language=target)
+
+  return(translation)
+
+if __name__ == '__main__':
+  file = open('./TotalEvent.txt', encoding='latin')
+  eventDf = pd.DataFrame.from_csv(file, index_col=None, sep='\t')
+  trans = []
+  for idx, text in enumerate(eventDf['Events']):
+    translation = run_quickstart(text)
+    print(idx)
+    trans.append(translation['translatedText'])
