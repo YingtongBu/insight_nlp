bab55c70d3ace1971365c40641f01510f9191d99
xinjin12 <xin.jin12@pactera.com>
2018-08-21 10:14:37 -0700

    add SpeechRecognition.py

---
+++ XJ/SpeechRecognitionDemo/SpeechRecognition.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#coding: utf8
+#author: Xin Jin (xin.jin12@pactera.com)
+import speech_recognition as sr 
+
+if __name__ == '__main__':
+  r = sr.Recognizer()
+  mic = sr.Microphone(device_index=1)
+  with mic as source:
+    r.adjust_for_ambient_noise(source)
+    audio = r.listen(source, phrase_time_limit=10, timeout=5)
+  try:
+    print(r.recognize_google(audio, show_all=True, language="cmn-Hans-CN"))
+  except:
+    print("Sorry, I cannot understand your questions.")
