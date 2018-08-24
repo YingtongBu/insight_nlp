dc1be5525095d1fc38c072b13934f7e62ca9e141
Summer Xia <summer@macbook-pro.attlocal.net>
2018-08-24 00:00:33 -0700

    deleted:    Documents/coding.rule.v1.7.pdf   deleted:    Documents/manual.of.linux.vim.v1.0.pdf   modified:   Translation/Translate.py

--- Translation/Translate.py
+++ Translation/Translate.py
@@ -22,3 +22,8 @@ if __name__ == '__main__':
   src = "Beijing"
   tran = translate_sentence(src)
   print(f"{src} --> {tran}")
+  
+  src = "你好，平安银行"
+  tran = translate_sentence(src, "en")
+  print(f"{src} --> {tran}")
+  
