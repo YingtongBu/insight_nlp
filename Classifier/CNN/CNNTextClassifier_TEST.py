e5561e3f63eacef3b7ece06fd108d32f31ccaf89
meteorShower <yu.liu55@pactera.com>
2018-08-30 15:40:58 -0700

    modified:   CNNTextClassifier_TEST.py

--- Classifier/CNN/CNNTextClassifier_TEST.py
+++ Classifier/CNN/CNNTextClassifier_TEST.py
@@ -1,11 +1,7 @@
 #coding: utf8
 #author: Yu Liu (yu.liu55@pactera.com)
 
-import optparse
 from Insight_NLP.Classifier.CNN.CNNTextClassifier import CNNTextClassifier
-
-def main():
-  pass
 
 if __name__ == '__main__':
   cnn_project = CNNTextClassifier(GPU=3)
