d0c303b921161e2516b3f22e58e010184ec7d35d
Xinlu Yu <xinlu.yu1@pactera.com>
2018-08-31 16:38:45 -0700

    add manual for google api key 	modified:   Web/SearchEngine.py 	modified:   Web/SearchEngine_TEST.py

--- Web/SearchEngine_TEST.py
+++ Web/SearchEngine_TEST.py
@@ -4,7 +4,6 @@
 from Insight_NLP.Web.SearchEngine import SearchEngine
 
 if __name__ == '__main__':
-  searcher = SearchEngine(custom_search_key='005808576341306023160:yojc6z7o63u',
-                      developer_key="AIzaSyC1o8pJAwMvaRugaRp9nWtvrGQs2_llEps")
+  searcher = SearchEngine()
   result = searcher.search('拼多多', item_nums=100)
   print(result)
