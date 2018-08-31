beea37281dc9e9c665079a4c2ae460c085004859
Xinlu Yu <xinlu.yu1@pactera.com>
2018-08-31 15:55:20 -0700

    change format 	modified:   Web/SearchEngine.py 	modified:   Web/SearchEngine_TEST.py

--- Web/SearchEngine.py
+++ Web/SearchEngine.py
@@ -4,28 +4,33 @@
 from googleapiclient.discovery import build
 
 class SearchEngine(object):
-  def __init__(self, key='005808576341306023160:yojc6z7o63u'):
-    self.key = key
+  def __init__(self, custom_search_key='005808576341306023160:yojc6z7o63u',
+               developer_key="AIzaSyC1o8pJAwMvaRugaRp9nWtvrGQs2_llEps"):
+    self.customSearchkey = custom_search_key
+    self.developerKey = developer_key
 
-  def search(self, keywords:str, page_num=2):
+
+  def search(self, keywords:str, item_nums=100):
     """
     :param keywords:
-    :param page_num:
+    :param item_num:
     :return: [{"link": "www.sina.com.cn", "snippet": "today...."}, {...}]
     """
-    service = build("customsearch", "v1",
-                    developerKey="AIzaSyC1o8pJAwMvaRugaRp9nWtvrGQs2_llEps")
+    service = build("customsearch", "v1",developerKey=self.developerKey)
     result = []
-    for i in range(page_num):
+    page_nums = item_nums // 10
+    if item_nums % 10 > 0:
+      page_nums = page_nums + 1
+    for i in range(page_nums):
       # q: search item
       # cx: search engine key
       # num: search items returned in each search
       # highRange: the last item number of search result, used for paging
       res = service.cse().list(
         q=keywords,
-        cx=self.key,
+        cx=self.customSearchkey,
         num=10,
-        highRange=(i + 1) * 10
+        highRange=(i+1)*10
       ).execute()
       for i in range(len(res['items'])):
         snippet = res['items'][i]['snippet'].replace('\n', '')
