6be54b18e2f0f5850e9b2e3048506d7fe27e075b
meteorShower <yu.liu55@pactera.com>
2018-09-03 22:25:38 -0700

    modified:   Classifier/CNN/_CNNTextClassifier_TEST.py

--- Classifier/CNN/_CNNTextClassifier_TEST.py
+++ Classifier/CNN/_CNNTextClassifier_TEST.py
@@ -1,10 +1,8 @@
 #coding: utf8
 #author: Yu Liu (yu.liu55@pactera.com)
 
-from Insight_NLP.Classifier.CNN.CNNTextClassifier import CNNTextClassifier
-# from Insight_NLP.Common import get_module_path
+from Insight_NLP.Classifier.CNN.CNNTextClassifier import CNNTextClassifiergi
 
 if __name__ == '__main__':
-  # get_module_path('Insight_NLP.Classifier.CNN.CNNTextClassifier')
   cnn_project = CNNTextClassifier(GPU=3)
   cnn_project.train()
