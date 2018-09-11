b06140d918dad3597c24d204c00d4e141821403e
Summer Xia <summer.xia1@pactera.com>
2018-09-10 17:26:36 -0700

    new file:   Documents/tensorflow.api.data   modified:   Tensorflow.py   new file:   _Tensorflow_TEST.py

--- Tensorflow.py
+++ Tensorflow.py
@@ -21,3 +21,35 @@ def tf_multi_hot(x, depth):
                                           tf.TensorShape([None, depth])])
 
   return v[1:,]
+
+
+def log_sum(tensor_list):
+  '''
+  :param tensor_list: All tensors are of [batch, 1] shape.
+  '''
+  tensor = tf.concat(tensor_list, 1)
+  return tf.reduce_logsumexp(tensor, 1)
+
+def create_bi_LSTM(word_ids,
+                   vob_size,
+                   embedding_size,
+                   LSTM_layer_num):
+  def encode(input, reuse):
+    with tf.variable_scope(tf.get_variable_scope(), reuse=reuse):
+      embeddings = tf.get_variable("embeddings", [vob_size, embedding_size])
+      cell = rnn_cell.MultiRNNCell([rnn_cell.LSTMCell(embedding_size)
+                                    for _ in range(LSTM_layer_num)])
+      word_vec = tf.nn.embedding_lookup(embeddings, input)
+      word_list = tf.unstack(word_vec, axis=1)
+      outputs, encoding = tf.nn.static_rnn(cell, word_list, dtype=tf.float32)
+      return outputs
+  
+  rnn_cell = tf.nn.rnn_cell
+  outputs1 = encode(word_ids, reuse=False)
+  
+  rev_words_ids = [reversed(list(sample)) for sample in word_ids]
+  outputs2 = encode(rev_words_ids, reuse=True)
+  outputs2 = list(reversed(outputs2))
+  
+  outputs = [tf.concat(o, axis=1) for o in zip(outputs1, outputs2)]
+  return outputs
---
+++ _Tensorflow_TEST.py
@@ -0,0 +1,21 @@
+#coding: utf8
+#author: Tian Xia (summer.xia1@pactera.com)
+
+from Insight_NLP.Common import *
+from Insight_NLP.Tensorflow import *
+
+if __name__ == "__main__":
+  parser = OptionParser(usage="cmd dev1@dir1 dir2")
+  #parser.add_option("-q", "--quiet", action = "store_true", dest = "verbose",
+  #default = False, help = "")
+  (options, args) = parser.parse_args()
+
+  sess = tf.Session()
+  x = tf.constant([[0.,],
+                   [1.,]])
+  y = tf.constant([[2.,],
+                   [3.,]])
+  output = log_sum([x, y])
+  print(sess.run(output))
+
+  
