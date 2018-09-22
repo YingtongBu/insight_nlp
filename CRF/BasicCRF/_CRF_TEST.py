51ad3dcf4bcbc4bc582002286a876345b461ec9e
Summer Xia <summer.xia1@pactera.com>
2018-09-21 17:16:21 -0700

    renamed:    CRF/BasicCRF/Data/test.data -> CRF/BasicCRF/SampleData/test.data   renamed:    CRF/BasicCRF/Data/train.pydict -> CRF/BasicCRF/SampleData/train.pydict   modified:   CRF/BasicCRF/_CRF_TEST.py

--- CRF/BasicCRF/_CRF_TEST.py
+++ CRF/BasicCRF/_CRF_TEST.py
@@ -6,29 +6,33 @@ import optparse
 from CRF.BasicCRF.FeatureExtraction import FeatureExtraction
 from CRF.BasicCRF.CRFTrainer import CRFTrainer
 from CRF.BasicCRF.CRFPredictor import CRFPredictor
+from Common import *
 
 if __name__ == '__main__':
   usage = "usage: %prog [options]"
   parser = optparse.OptionParser(usage=usage)
-  parser.add_option('--model_name',
-                    default='company')
-  parser.add_option('--data_for_train',
-                    default='./Data/train.pydict')
-  parser.add_option('--file_for_prediction',
-                    default='./Data/test.data')
   (options, args) = parser.parse_args()
+  
+  model_name = "model.company"
+  data_path = os.path.join(
+    get_module_path("Common"),
+    "CRF/BasicCRF/SampleData"
+  )
+  
+  train_data = os.path.join(data_path, "train.pydict")
+  test_data  = os.path.join(data_path, "test.data")
 
   #train
-  crf_trainer = CRFTrainer(options.model_name,
-                           options.data_for_train,
+  crf_trainer = CRFTrainer(model_name,
+                           train_data,
                            FeatureExtraction(), 0.1, 0.01, 200)
   crf_trainer.train()
 
   #predictor_init
-  crf_predictor = CRFPredictor(options.model_name, FeatureExtraction())
+  crf_predictor = CRFPredictor(model_name, FeatureExtraction())
 
   #predict a sample
   crf_predictor.predict('1.当事人:尉氏县第三人民医院', 'output_text.result')
 
   #predict in batch
-  crf_predictor.predict_file(options.file_for_prediction, 'output_file.result')
+  crf_predictor.predict_file(test_data, 'output_file.result')
