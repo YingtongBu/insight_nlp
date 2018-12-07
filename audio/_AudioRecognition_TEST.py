a9a8319c98d8113fa75ae8a15bfd5ee02d3c53a9
Tian Xia <summer@MacBook-Pro-2nd-gen.local>
2018-12-06 20:07:39 -0800

    modified:   audio/_AudioRecognition_TEST.py

--- audio/_AudioRecognition_TEST.py
+++ audio/_AudioRecognition_TEST.py
@@ -2,18 +2,14 @@
 #coding: utf8
 #author: Xinlu Yu (xinlu.yu1@pactera.com)
 
-import audio.AudioConverter import AudioHelper
+import audio.SpeechRecognition as asr
 import Common as cm
 import os
 
 if __name__ == '__main__':
   data_path = os.path.join(
     cm.get_module_path("Common"),
-    "audio/test_data",
+    "audio/test.wav",
   )
 
-  files = cm.get_files_in_folder(data_path,
-                                 file_extensions=AudioHelper.)
-
-  SpeechRecognition.audio_file_recognize(data_path,
-                                         language_selection="Zh-cn")
+  asr.audio_file_recognize(data_path, language_selection="Zh-cn")
