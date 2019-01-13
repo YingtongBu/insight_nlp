92a7128620c40381ae722007935e1a02f053c4e2
Tian Xia <SummerRainET2008@gmail.com>
2019-01-13 01:05:59 -0800

    modified:   audio/microphone_TEST.py

--- audio/microphone_TEST.py
+++ audio/microphone_TEST.py
@@ -6,7 +6,7 @@ def main():
   #parser.add_option("-q", "--quiet", action="store_true", dest="verbose",
                      #default=False, help="")
   parser.add_option("--time", type=int, default=5, help="default 5 seconds")
-  parser.add_option("--export_wav_file", default="microphone.wav",
+  parser.add_option("--export_wav_file", default="/tmp/microphone.wav",
                     help="default microphone.wav")
   (options, args) = parser.parse_args()
 
