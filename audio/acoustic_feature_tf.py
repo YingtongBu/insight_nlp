351fc4101d4ee06617d1a0505f86b2d63aa9e419
Tian Xia <SummerRainET2008@gmail.com>
2019-01-20 14:56:19 -0800

    modified:   audio/acoustic_feature_tf.py

--- audio/acoustic_feature_tf.py
+++ audio/acoustic_feature_tf.py
@@ -9,37 +9,38 @@ class DataGraphMFCC:
   stride: int=480
 
   def __init__(self, dct_coef_count: int=13):
-   '''
-   By default settings, the returned dimension would be [100, 13], [100, 13],
-   [100, 13]
-   '''
-   self._graph = tf.Graph()
-   with self._graph.as_default():
-     self._wav_file_ts = tf.placeholder(tf.string, [], name='wav_filename')
-     self._frame_num = tf.placeholder(tf.int32, [])
-     wav_loader = io_ops.read_file(self._wav_file_ts)
-     wav_decoder = contrib_audio.decode_wav(wav_loader, desired_channels=1)
-     audio_clamp = tf.clip_by_value(wav_decoder.audio, -1.0, 1.0)
-     spectrogram = contrib_audio.audio_spectrogram(
-       audio_clamp,
-       window_size=self.window_size,
-       stride=self.stride,
-       magnitude_squared=True)
+    '''
+    By default settings, the returned dimension would be [100, 13], [100, 13],
+    [100, 13]
+    '''
+    self._graph = tf.Graph()
+    with self._graph.as_default():
+      self._wav_file_ts = tf.placeholder(tf.string, [], name='wav_filename')
+      self._frame_num = tf.placeholder(tf.int32, [])
+      wav_loader = io_ops.read_file(self._wav_file_ts)
+      wav_decoder = contrib_audio.decode_wav(wav_loader, desired_channels=1)
+      audio_clamp = tf.clip_by_value(wav_decoder.audio, -1.0, 1.0)
+      spectrogram = contrib_audio.audio_spectrogram(
+        audio_clamp,
+        window_size=self.window_size,
+        stride=self.stride,
+        magnitude_squared=True)
 
-     feat_ts = contrib_audio.mfcc(
-       spectrogram=spectrogram,
-       sample_rate=wav_decoder.sample_rate,
-       dct_coefficient_count=dct_coef_count,
-     )
-     self._feat_ts = feat_ts[0]
-     self._real_length = tf.shape(self._feat_ts)[0]
+      feat_ts = contrib_audio.mfcc(
+        spectrogram=spectrogram,
+        sample_rate=wav_decoder.sample_rate,
+        dct_coefficient_count=dct_coef_count,
+      )
+      self._feat_ts = feat_ts[0]
+      self._real_length = tf.shape(self._feat_ts)[0]
 
-     self._expanded_feat_ts = tf.pad(
-       self._feat_ts,
-       [[0, self._frame_num - self._real_length], [0, 0]],
-     )
+      self._expanded_feat_ts = tf.pad(
+        self._feat_ts,
+        [[0, self._frame_num - self._real_length], [0, 0]],
+      )
 
-   self._sess = tf.Session(graph=self._graph)
+    self._sess = tf.Session(graph=self._graph)
+    print(f"DataGgraphMFCC graph is created!")
 
   def run(self, wav_file: str, target_frame_num: int=-1):
     assert nlp.get_file_extension(wav_file) == "wav"
