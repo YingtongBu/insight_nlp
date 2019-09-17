e1abefb8b804f956371dc855c58a530c9f25ea59
Tian Xia <SummerRainET2008@gmail.com>
2019-09-17 15:11:36 -0700

    new file:   pa_nlp/tf/__init__.py   new file:   pa_nlp/tf/estimator/dataset.py   new file:   pa_nlp/tf/estimator/dataset_TEST.py

---
+++ pa_nlp/tf/estimator/dataset.py
@@ -0,0 +1,79 @@
+#author: Tian Xia (summer.xia1@pactera.com)
+
+from pa_nlp.tf.estimator import *
+from pa_nlp.tf.estimator import Param
+
+# Buffer size for reading records from a TFRecord file. Each training file is
+# 7.2 MB, so 8 MB allows an entire file to be kept in memory.
+_READ_RECORD_BUFFER = 64 * 1000 * 1000
+
+class DataReader(abc.ABC):
+  def __init__(self,
+               tfrecord_file: str, param: Param,
+               training: bool, debug: bool=False):
+    self._tf_file = tfrecord_file
+    self._param = param
+    self._shuffle = training
+    self._debug = debug
+
+  @abc.abstractmethod
+  def parse_example(self, serialized_example):
+    '''return parsed tensors
+    '''
+    pass
+
+  def _batch_examples(self, dataset):
+    '''you could define how to group examples'''
+
+    dataset = dataset.apply(
+      tf.data.experimental.map_and_batch(
+        map_func=lambda example: self.parse_example(example),
+        batch_size=self._param.batch_size,
+        drop_remainder=False,
+      )
+    )
+
+    return dataset
+
+  def _read_and_batch_from_files(self):
+    dataset = tf.data.Dataset.list_files(self._tf_file, shuffle=self._shuffle)
+
+    dataset = dataset.apply(
+      tf.contrib.data.parallel_interleave(
+        lambda tf_file: tf.data.TFRecordDataset(
+          tf_file, buffer_size=_READ_RECORD_BUFFER
+        ),
+        sloppy=self._shuffle, cycle_length=4
+      )
+    )
+
+    dataset = self._batch_examples(dataset)
+    dataset = dataset.repeat(1)
+    dataset = dataset.prefetch(buffer_size=tf.contrib.data.AUTOTUNE)
+    data_iter = dataset.make_initializable_iterator()
+    sample = data_iter.get_next()
+
+    return data_iter.initializer, sample
+
+  def get_batch_data(self):
+    graph = tf.Graph()
+    with graph.as_default():
+      initializer, sample = self._read_and_batch_from_files()
+    sess = tf.Session(graph=graph)
+
+    for epoch_id in range(self._param.epoch_num):
+      sess.run(initializer)
+
+      while True:
+        try:
+          start_time = time.time()
+          batch_data = sess.run(sample)
+          duration = time.time() - start_time
+          if self._debug:
+            print(f"batch fetching time: {duration:.4f} seconds.")
+
+          yield epoch_id, batch_data
+
+        except tf.errors.OutOfRangeError:
+          break
+
---
+++ pa_nlp/tf/estimator/dataset_TEST.py
@@ -0,0 +1,77 @@
+#author: Tian Xia (summer.xia1@pactera.com)
+#!/usr/bin/env python3
+
+from pa_nlp.tf.estimator import *
+from pa_nlp.tf.estimator.dataset import DataReader
+from pa_nlp.tf.estimator import Param
+
+def write_file(tf_file: str):
+  class Serializer:
+    def __call__(self, seg_samples: list):
+      for sample in seg_samples:
+        name, age, height = sample
+        feature = {
+          "name": nlp_tf.tf_feature_bytes(name.encode("utf8")),
+          "age": nlp_tf.tf_feature_int64(age),
+          "height": nlp_tf.tf_feature_float(height),
+        }
+        example_proto = tf.train.Example(
+          features=tf.train.Features(feature=feature)
+        )
+
+        yield example_proto.SerializeToString()
+
+  def get_file_record():
+    data = [
+      ["summer", 30, 176],
+      ["xia", 34, 185],
+      ["tian", 45, 156],
+      ["fang", 21, 165],
+      ["hua", 20, 160],
+      ["rain", 40, 170],
+    ]
+    for sample in data:
+      yield [sample]
+
+  nlp_tf.write_tfrecord(get_file_record(), Serializer(), tf_file)
+
+def read_file(tf_file: str):
+  class MyDataReader(DataReader):
+    def parse_example(self, serialized_example):
+      data_fields = {
+        "name": tf.FixedLenFeature((), tf.string, ""),
+        "age": tf.FixedLenFeature((), tf.int64, 0),
+        "height": tf.FixedLenFeature((), tf.float32, 0),
+      }
+      parsed = tf.parse_single_example(serialized_example, data_fields)
+
+      name = parsed["name"]
+      age = parsed["age"]
+      height = parsed["height"]
+
+      return name, age, height
+
+  param = Param()
+  param.batch_size = 3
+  param.epoch_num = 2
+
+  data_reader = MyDataReader(tf_file, param, True, True)
+  for epoch_id, batch_data in data_reader.get_batch_data():
+    print(epoch_id, batch_data)
+
+def main():
+  parser = optparse.OptionParser(usage="cmd [optons] ..]")
+  # parser.add_option("-q", "--quiet", action="store_true", dest="verbose",
+  parser.add_option("--gpu", default="-1", help="default=-1")
+
+  # default=False, help="")
+  (options, args) = parser.parse_args()
+  print(options)
+  os.environ["CUDA_VISIBLE_DEVICES"] = options.gpu
+
+  tf_file = "_debug.tfrecord"
+  write_file(tf_file)
+  read_file(tf_file)
+
+if __name__ == '__main__':
+  main()
